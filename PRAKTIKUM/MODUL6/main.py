2c6b7af883dcb01cf19d3c3c1fa70a1907b5334f
raflikharismaa <raflikharisma29@webmail.umm.ac.id>
2023-12-20 16:52:34 +0700

    chore: finishinh all modules

--- PRAKTIKUM/MODUL6/main.py
+++ PRAKTIKUM/MODUL6/main.py
@@ -3,7 +3,6 @@ from rembg import remove
 
 background = Image.open("D:\\KULIAH\\SEMESTER 5\\FUNGSIONAL\\PRAKTIKUM\\Click Here\\PRAKTIKUM\\MODUL6\\umm.jpg")
 overlay = Image.open("D:\\KULIAH\\SEMESTER 5\\FUNGSIONAL\\PRAKTIKUM\\Click Here\\PRAKTIKUM\\MODUL6\\umm_overlay.png")
-
 font = "D:\KULIAH\SEMESTER 5\FUNGSIONAL\PRAKTIKUM\Click Here/font\ARIBL0.ttf"
 
 background = background.resize((1920,1080))
@@ -12,22 +11,15 @@ rotated = bg_copy.rotate(30)
 final_bg = rotated.filter(ImageFilter.BLUR)
 
 
-
 #NIM 402
 enhancer = ImageEnhance.Brightness(overlay.copy())
 brightened = enhancer.enhance(1.2)
-
 enhancer = ImageEnhance.Contrast(brightened)
 effect = enhancer.enhance(1.2)
 # model = effect.rotate(30)
 final = effect.resize((600,600))
 
-
- 
-
-
 padding = 40
-
 customFont = ImageFont.truetype(font, 24)
 draw = ImageDraw.Draw(final)
 text = "INFORMATIKA JOSSS"
@@ -36,7 +28,6 @@ text_width, text_height = draw.textsize(text, font=customFont)
 text_position = ((bg_width - text_width) // 2, bg_height - text_height - padding)
 draw.text(text_position,text, font = customFont, fill="black")
 final_bg.paste(final, (600,300))
-
 final_bg.show()
 
 
